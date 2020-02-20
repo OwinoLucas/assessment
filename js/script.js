adb3ce60d0dfc63c6ccff3d58922b5afbdb861e6
Lucas Owino <lucowish35@gmail.com>
2020-02-20 16:47:14 +0300

    Add code to javascript

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function myFunc (myString = ["foo", "foo4", 30, 100]){
+    for (i= 0; i < myString.length; i+= 1){
+    if (myStrings.endsWith(0,1,2,3,4,5) === false){
+    myStrings.push(1);
+    }
+    else {
+    myString ++
+    }
+    }
+    }
+    
